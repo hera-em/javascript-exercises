680d7257bb0f43d26f0d32d026fbf493363e9d68
hera <165401313+hera-em@users.noreply.github.com>
2025-12-09 20:32:15 +0400

    Handle negative values in 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,6 @@
 const repeatString = function(string, num) {
     let repeatedString = "";
+    if (num < 0) return "ERROR";
     for (let i = 0; i < num; i++) {
         repeatedString = repeatedString + string;    
     }
