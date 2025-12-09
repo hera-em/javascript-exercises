088f6f3698228290588c00569e366cadb33d9439
hera <165401313+hera-em@users.noreply.github.com>
2025-12-09 20:30:43 +0400

    Solve 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatedString = "";
+    for (let i = 0; i < num; i++) {
+        repeatedString = repeatedString + string;    
+    }
+    return repeatedString;
 };
+
+console.log(repeatString("",3));
 
 // Do not edit below this line
 module.exports = repeatString;
