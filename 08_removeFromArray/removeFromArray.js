dc767c16d14f41f1172bf424d13cb9e1e719fe35
hera <165401313+hera-em@users.noreply.github.com>
2025-12-11 18:50:34 +0400

    Solve 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+
+    return arr.filter((element) => !nums.includes(element));
+
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
