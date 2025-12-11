77d27f1f7a16d4ba2bdf5b3bb20e34b23a4da8e9
hera <165401313+hera-em@users.noreply.github.com>
2025-12-11 19:48:21 +0400

    Solve 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let smallerNum = 0;
+    let largerNum = 0;
+    let sum = 0;
+
+    const args = Array.from(arguments);
+
+    for (let i = 0; i < args.length; i++) {
+        if (args[i] < 0 || 
+            args[i] % 1 != 0 || 
+            Number.isNaN(args[i]) || 
+            args[i] === args[i].toString()) {
+            return "ERROR";
+        }
+        
+    }
+
+    if (num1 > num2) {
+        largerNum = num1;
+        smallerNum = num2;
+    }
+    else {
+        largerNum = num2;
+        smallerNum = num1;
+    }
+
+    for (let i = smallerNum; i <= largerNum; i++) {
+        sum += i;
+    }
+    
+    return sum;
 
 };
 
