e474be7082b15422f2316c596e34253bbeb1b5d9
hera <165401313+fixmakintosh@users.noreply.github.com>
2026-02-20 21:54:39 +0400

    Solve fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,28 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let number = Number (a);
+    if (number < 0) {
+        return "OOPS";
+    }
+    if (number === 0) {
+        return 0;
+    }
+    let sequence = [];
+    let cache1 = 1;
+    let cache2 = 1;
+    let cache3 = 1;
+    for (let i = 1; i <= number; i++) {
+        if (i == 1 || i == 2) {
+            sequence.push(1);
+        }
+        else {
+            cache3 = cache1 + cache2;
+            sequence.push(cache3);
+            cache2 = cache1;
+            cache1 = cache3;
+            
+        }
+    }
+    return sequence.at(number-1);
 };
 
 // Do not edit below this line
