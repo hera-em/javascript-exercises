a76cb3e3ead37600e551e2f766acd509138a8c74
hera <165401313+fixmakintosh@users.noreply.github.com>
2026-02-20 20:58:19 +0400

    Try to solve palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let lowerCaseString = string.toLowerCase();
+    const punctuation = ['.', ',', ';', ':', '!', '?', '"', "'", '(', ')', '[', ']', '{', '}', '-', '_', " "];
+    let rawString = "";
+    for (let i = 0; i < lowerCaseString.length; i++) {
+        if (!punctuation.includes(lowerCaseString[i])) {
+            rawString += lowerCaseString[i];
+        }
+    }
+    let middle = (Math.ceil(rawString.length / 2))
+    let firstHalf = Array.from(rawString.substring(0, middle - 1));
+    let secondHalf = Array.from(rawString.substring(middle));
+    console.log(rawString);
+    console.log (firstHalf);
+    console.log (secondHalf);
+    let secondHalfReverse = secondHalf.toReversed();
 
+    if (firstHalf.toString() === secondHalfReverse.toString()) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
+console.log(palindromes("a   boba!!"));
+
 
 // Do not edit below this line
 module.exports = palindromes;
--- foundations/13_palindromes/palindromes.spec.js
+++ foundations/13_palindromes/palindromes.spec.js
@@ -1,30 +1,6 @@
 const palindromes = require('./palindromes')
 
 describe('palindromes', () => {
-<<<<<<< HEAD
-  test('works with single words', () => {
-    expect(palindromes('racecar')).toBe(true);
-  });
-  test.skip('works with punctuation ', () => {
-    expect(palindromes('racecar!')).toBe(true);
-  });
-  test.skip('works with upper-case letters ', () => {
-    expect(palindromes('Racecar!')).toBe(true);
-  });
-  test.skip('works with multiple words', () => {
-    expect(palindromes('A car, a man, a maraca.')).toBe(true);
-  });
-  test.skip('works with multiple words', () => {
-    expect(palindromes('Animal loots foliated detail of stool lamina.')).toBe(true);
-  });
-  test.skip('doesn\'t just always return true', () => {
-    expect(palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
-  });
-  test.skip('works with numbers in a string', () => {
-    expect(palindromes('rac3e3car')).toBe(true);
-  });
-  test.skip('works with unevenly spaced numbers in a string', () => {
-=======
   test('detects palindrome', () => {
     expect(palindromes('racecar')).toBe(true);
   });
@@ -44,7 +20,6 @@ describe('palindromes', () => {
     expect(palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
   });
   test.skip('detects non-palindromes with numbers', () => {
->>>>>>> upstream/main
     expect(palindromes('r3ace3car')).toBe(false);
   });
 });
