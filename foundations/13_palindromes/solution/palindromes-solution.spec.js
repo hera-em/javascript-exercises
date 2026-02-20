6abcdf6dd9174c2aac3915a58201ee97d8cdc0ec
hera <165401313+fixmakintosh@users.noreply.github.com>
2026-02-20 21:54:50 +0400

    Resolve conflicts

--- foundations/13_palindromes/solution/palindromes-solution.spec.js
+++ foundations/13_palindromes/solution/palindromes-solution.spec.js
@@ -1,32 +1,6 @@
 const palindromes = require('./palindromes-solution');
 
 describe('palindromes', () => {
-<<<<<<< HEAD
-  test('works with single words', () => {
-    expect(palindromes('racecar')).toBe(true);
-  });
-  test('works with punctuation ', () => {
-    expect(palindromes('racecar!')).toBe(true);
-  });
-  test('works with upper-case letters ', () => {
-    expect(palindromes('Racecar!')).toBe(true);
-  });
-  test('works with multiple words', () => {
-    expect(palindromes('A car, a man, a maraca.')).toBe(true);
-  });
-  test('works with multiple words', () => {
-    expect(palindromes('Animal loots foliated detail of stool lamina.')).toBe(
-      true
-    );
-  });
-  test("doesn't just always return true", () => {
-    expect(palindromes('ZZZZ car, a man, a maraca.')).toBe(false);
-  });
-  test('works with numbers in a string', () => {
-    expect(palindromes('rac3e3car')).toBe(true);
-  });
-  test('works with unevenly spaced numbers in a string', () => {
-=======
   test('detects palindrome', () => {
     expect(palindromes('racecar')).toBe(true);
   });
@@ -46,7 +20,6 @@ describe('palindromes', () => {
     expect(palindromes('ZZZZ car, a man, a maracaz.')).toBe(false);
   });
   test('detects non-palindromes with numbers', () => {
->>>>>>> upstream/main
     expect(palindromes('r3ace3car')).toBe(false);
   });
 });
